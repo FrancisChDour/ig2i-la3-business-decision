051bf76f8dbd59d67523ccd4e10b59352f32ff08
FDOURL28 <francois.dourlensmonchy@decathlon.com>
2021-09-29 14:55:14 +0200

    Initial commit

---
+++ correlation_matrice.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv("fromages.csv")
+print(data)
+
+corr_df = data.corr()
+print("The correlation DataFrame is:")
+print(corr_df, "\n")
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(corr_df, annot=True)
+plt.show()
