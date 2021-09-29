0bd16256c7e8ab932e8647d0097142354fde6cea
FDOURL28 <francois.dourlensmonchy@decathlon.com>
2021-09-29 15:01:09 +0200

    correlation_matrice

--- correlation_matrice.py
+++ correlation_matrice.py
@@ -2,13 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# We create the data frame by importing the CSV file
 data = pd.read_csv("fromages.csv")
 print(data)
 
+# Then we create the correlation matrix
 corr_df = data.corr()
 print("The correlation DataFrame is:")
 print(corr_df, "\n")
 
+# Finally, we show the matrix with colors
 plt.figure(figsize=(8, 6))
 sns.heatmap(corr_df, annot=True)
 plt.show()
